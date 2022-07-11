17b0920b31cb32ae8a26669c3042caf98357b57e
effortsnu <whdgus1054@snu.ac.kr>
2022-07-11 15:51:46 +0900

    1

---
+++ lib/newmember.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+class newmember extends StatefulWidget {
+  const newmember({Key? key}) : super(key: key);
+
+  @override
+  State<newmember> createState() => _newmemberState();
+}
+
+class _newmemberState extends State<newmember> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("회원가입"),
+        backgroundColor: Colors.black,
+      ),
+      body: ,
+    );
+  }
+}
