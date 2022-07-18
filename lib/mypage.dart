70cd80aa2d59b1034f92c296adadd1322a68d8ce
joohyung990 <pjh990@snu.ac.kr>
2022-07-18 13:57:01 +0900

    마이페이지 초안

---
+++ lib/mypage.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("마이페이지"),
+          centerTitle: true,
+          backgroundColor: Colors.teal,
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage(),
+                ),
+                Column(
+                  children: [
+                    Text("박주형"),
+                    Text("서울대학교 기계항공공학부"),
+                    Text("다물 레벨: 골드"),
+                  ]
+                )
+              ]
+            ),
+            Row(
+              children: [
+                TextButton(
+                  child: Text("과외 일정보기"),
+                  onPressed: (){},
+                ),
+                TextButton(
+                  child: Text("프로필 수정"),
+                  onPressed: (){},
+                )
+              ]
+            )
+          ]
+
+        )
+
+    );
+  }
+  }
