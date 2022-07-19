206746feb622a75040a23230dcaa398a0157496d
effortsnu <whdgus1054@snu.ac.kr>
2022-07-19 18:58:19 +0900

    revision -ing

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'login.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
